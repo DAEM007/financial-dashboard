15bfa6a1ce79d1307fd15986fc7dc7eb0e0f858e
DAMILOLA <dammylola299@gmail.com>
2024-01-07 01:46:01 +0100

    tested empty array in the invoice Id fetch

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -167,7 +167,7 @@ export async function fetchInvoiceById(id: string) {
       amount: invoice.amount / 100,
     }));
 
-    console.log(invoice);
+    // console.log(invoice); // empty array
     
     return invoice[0];
   } catch (error) {
